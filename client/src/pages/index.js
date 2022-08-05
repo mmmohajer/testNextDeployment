13176282882e833ee8d31d2e3f074de5a2cbe94d
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-08-05 13:28:48 -0400

    Favicon added

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import { Div } from 'basedesign-iswad';
 
+import Seo from '@/baseComponents/Seo';
 import PublicRoute from '@/components/PublicRoute';
 import Introduction from '@/components/Introduction';
 import Contact from '@/components/Contact';
@@ -14,20 +15,22 @@ import styles from './index.module.scss';
 function Home() {
   return (
     <PublicRoute>
-      <Introduction />
-      <About />
-      <Services />
+      <Seo>
+        <Introduction />
+        <About />
+        <Services />
 
-      <Div className="mt8 w-per-100 bgInverse py3" id="Contact">
-        <Div className="show-block-in-md-lg w-per-50 ml-auto mr-auto">
-          <Contact />
+        <Div className="mt8 w-per-100 bgInverse py3" id="Contact">
+          <Div className="show-block-in-md-lg w-per-50 ml-auto mr-auto">
+            <Contact />
+          </Div>
+          <Div className="show-block-in-sm-xsm ml-auto mr-auto">
+            <Contact />
+          </Div>
         </Div>
-        <Div className="show-block-in-sm-xsm ml-auto mr-auto">
-          <Contact />
-        </Div>
-      </Div>
 
-      <Testimonials />
+        <Testimonials />
+      </Seo>
     </PublicRoute>
   );
 }
